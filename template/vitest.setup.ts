5bf58a0c0a7697e7cefca0967a8ad25d6ec29952
xuyingzhou <xuyingzhou@tuzhanai.com>
2026-05-05 00:56:58 +0800

    fix: add WebSocket mock for Node.js test environment

--- template/vitest.setup.ts
+++ template/vitest.setup.ts
@@ -41,3 +41,22 @@ if (typeof window !== 'undefined') {
 
 // EventSource can be used in both environments
 global.EventSource = EventSource as unknown as typeof globalThis.EventSource
+
+if (typeof globalThis.WebSocket === 'undefined') {
+  class MockWebSocket {
+    static CONNECTING = 0
+    static OPEN = 1
+    static CLOSING = 2
+    static CLOSED = 3
+    readyState = MockWebSocket.CONNECTING
+    send() {}
+    close() {}
+    addEventListener() {}
+    removeEventListener() {}
+    dispatchEvent() {
+      return true
+    }
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  globalThis.WebSocket = MockWebSocket as any
+}
